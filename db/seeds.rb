56e052cfed4db8b407d0393874b56e57f089f658
Yassine Omari <yassine@oddcamp.com>
2023-03-02 14:20:21 +0100

    feat: Load the data from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+
+def csv_file_path
+  @csv_file_path ||= File.read('CRM_data.csv')
+end
+
+def csv
+  @csv ||= CSV.parse(csv_file_path, col_sep: ",", row_sep: :auto, skip_blanks: true, headers: true)
+end
+
+csv.each do |row|
+  unless Company.find_by(name: row[5]) || !row[5]
+    Company.create!(name: row[5])
+  end
+
+  Employee.create!(first_name: row[0], last_name: row[1], email: row[2], 
+                stage:row[3], phone_number: row[4], 
+                company: Company.find_by(name: row[5]), 
+                probability: row[6] || 0 )
+end
+
+p 'done!!!'
