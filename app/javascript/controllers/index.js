23e158fb78e91fe1380a96d798524f94c49724cd
Yassine Omari <yassine@oddcamp.com>
2023-03-05 09:45:28 +0100

    feat: Set the views

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import SearchFormController from "./search_form_controller"
+application.register("search-form", SearchFormController)
+
+import RemovalsController from './removals_controller.js';
+application.register('removals', RemovalsController);
