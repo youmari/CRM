cffc3310f21b2203ec28048bf0851c01cba31831
Yassine Omari <yassine@oddcamp.com>
2023-03-05 09:49:05 +0100

    feat: Run rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'clients#index'
+
+  resources :clients, except: %i[show] do
+    get :list, on: :collection, action: :filtered_list
+  end
+
+  resources :companies, only: %i[new create]
 end
