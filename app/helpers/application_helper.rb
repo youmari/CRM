02826030bcc3f1eb26506ab6bef5d87a9e539669
Yassine Omari <yassine@oddcamp.com>
2023-03-05 09:48:33 +0100

    feat: Add falsh messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  include Pagy::Frontend
+
+  def render_turbo_stream_flash_messages
+    turbo_stream.prepend 'flash', partial: 'shared/flash'
+  end
 end
